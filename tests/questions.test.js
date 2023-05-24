5296c841e3294112ae5ebe26c7ca49f497728ab4
Frank-Colorado <chawnkiee@gmail.com>
2023-05-24 19:26:42 -0400

    commeted question.js

--- tests/questions.test.js
+++ tests/questions.test.js
@@ -1,14 +1,16 @@
 // require the validateColor function from the questions.js file
 const { validateColor } = require("../libs/questions");
 
+// A test the validateColor function
 describe("validateColor", () => {
+  // A test that checks if the validateColor function returns true for a valid color keyword or hexadecimal code
   test("should return true for a valid color keyword or hexadecimal code", () => {
     const result = validateColor("red");
     expect(result).toBe(true);
     const result2 = validateColor("#ff0000");
     expect(result2).toBe(true);
   });
-
+  // A test that checks if the validateColor function returns an error message for an invalid color keyword or hexadecimal code
   test("should return an error message for an invalid color keyword or hexadecimal code", () => {
     const result = validateColor("invalid");
     expect(result).toBe("Please enter a valid color");
