cbca780456e8afeb22c172bec330b232c4c9d85d
Frank-Colorado <chawnkiee@gmail.com>
2023-05-24 17:06:48 -0400

    required packages and modules in index.js file

---
+++ index.js
@@ -0,0 +1,6 @@
+// require the inquirer package
+const inquirer = require("inquirer");
+// require the fs package
+const fs = require("fs");
+// require the shape classes from the shapes.js file
+const { Circle, Square, Triangle } = require("./libs/shapes");
