7813765fc8732e9e7f3e536c0013d86250cd2ce8
Frank-Colorado <chawnkiee@gmail.com>
2023-05-24 17:49:21 -0400

    created a test for Triangle class

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -35,3 +35,21 @@ describe("Square", () => {
     });
   });
 });
+
+// A test for the Triangle class
+describe("Triangle", () => {
+  // A test for the render method
+  describe("render", () => {
+    it("should return an svg string for a triangle", () => {
+      const triangle = new Triangle("test", "red", "blue");
+      const result = triangle.render();
+      expect(result).toEqual(expect.any(String));
+      expect(result).toContain(`
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+          <polygon points="50,10 10,90 90,90" fill="blue"/>
+          <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="red">test</text>
+          </svg>
+          `);
+    });
+  });
+});
