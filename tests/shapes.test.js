974f7adffa69b9fe42f8b99300773afa39355d26
Frank-Colorado <chawnkiee@gmail.com>
2023-05-24 19:11:04 -0400

    refactored shape tests

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,55 +1,48 @@
 // require the shape classes from the shapes.js file
 const { Circle, Square, Triangle } = require("../libs/shapes");
 
-// A test for the Circle class
-describe("Circle", () => {
-  // A test for the render method
-  describe("render", () => {
-    it("should return an svg string for a circle", () => {
+describe("Shape classes", () => {
+  // A test for the Circle class
+  describe("Circle", () => {
+    // A test for the render method
+    test("should return an SVG string for a circle", () => {
       const circle = new Circle("test", "red", "blue");
       const result = circle.render();
-      expect(result).toEqual(expect.any(String));
-      expect(result)
-        .toContain(`<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-      <circle cx="50" cy="50" r="40" fill="blue"/>
-      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="red">test</text>
-      </svg>`);
+      expect(result).toContain('<circle cx="50" cy="50" r="40" fill="blue"/>');
+      expect(result).toContain(
+        '<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="red">test</text>'
+      );
     });
   });
-});
 
-// A test for the Square class
-describe("Square", () => {
-  // A test for the render method
-  describe("render", () => {
-    it("should return an svg string for a square", () => {
+  // A test for the Square class
+  describe("Square", () => {
+    // A test for the render method
+    test("should return an svg string for a square", () => {
       const square = new Square("test", "red", "blue");
       const result = square.render();
-      expect(result).toEqual(expect.any(String));
-      expect(result).toContain(`
-          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-          <rect x="10" y="10" width="80" height="80" fill="blue"/>
-          <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="red">test</text>
-          </svg>
-          `);
+
+      expect(result).toContain(
+        '<rect x="10" y="10" width="80" height="80" fill="blue"/>'
+      );
+      expect(result).toContain(
+        '<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="red">test</text>'
+      );
     });
   });
-});
 
-// A test for the Triangle class
-describe("Triangle", () => {
-  // A test for the render method
-  describe("render", () => {
-    it("should return an svg string for a triangle", () => {
+  // A test for the Triangle class
+  describe("Triangle", () => {
+    // A test for the render method
+    test("should return an svg string for a triangle", () => {
       const triangle = new Triangle("test", "red", "blue");
       const result = triangle.render();
-      expect(result).toEqual(expect.any(String));
-      expect(result).toContain(`
-          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-          <polygon points="50,10 10,90 90,90" fill="blue"/>
-          <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="red">test</text>
-          </svg>
-          `);
+      expect(triangle.render()).toContain(
+        '<polygon points="50,10 10,90 90,90" fill="blue"/>'
+      );
+      expect(result).toContain(
+        '<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="red">test</text>'
+      );
     });
   });
 });
